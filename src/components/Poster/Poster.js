2184fff36d43c80b0b00f53c0d30c19d15a579b2
Artem Kriachko <kriachkoartem@gmail.com>
2023-04-08 11:14:34 +0300

    Improve Design: Poster add border-radius; add scroll-animation to border-radius

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,14 +1,24 @@
 import './styles/poster.css'
+import {motion, useScroll, useTransform} from "framer-motion";
+
 
 export function Poster ({title = '', description = '', style = {}, children}) {
 
+   const {scrollY} = useScroll();
+   const offsetY = [-150, 300];
+   const borderRadius = ['0%', '50%'];
+   const styles = {
+      borderBottomLeftRadius: useTransform(scrollY, offsetY, borderRadius),
+      borderBottomRightRadius: useTransform(scrollY, offsetY, borderRadius),
+   };
+
   return (
      <section className={'poster'} style={style.poster}>
-        <div className="img-holder background">
-           <div className="background">
+        <motion.div className="img-holder background" style={styles}>
+           <div className="background-img">
               Poster
            </div>
-        </div>
+        </motion.div>
         <div className="container">
            <div className="posterContent" style={style.content}>
               <h1 className={'title'}>{title}</h1>
