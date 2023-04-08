4889d9acdc55ac36bfc7cd1a692dabfc69ff0ac9
Artem Kriachko <kriachkoartem@gmail.com>
2023-04-08 11:01:08 +0300

    Improve Design: Header

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,6 +1,6 @@
 import './styles/header.css'
 import {Link, NavLink} from "react-router-dom";
-import { motion, useTransform} from "framer-motion";
+import {motion, useTransform} from "framer-motion";
 import {CoursesListModal} from "../../components/CoursesListModal/CoursesListModal";
 import {IoChevronDownOutline} from "react-icons/io5";
 
@@ -8,38 +8,34 @@ import {IoChevronDownOutline} from "react-icons/io5";
 export function Header({offsetY, scrollY}) {
 
 
-   const headerSizes = [100, 50];
-   const styles = {
-      height: useTransform(scrollY, offsetY, headerSizes),
-   };
-
-
    return (
-      <motion.header style={styles}>
-         <Link to={'/'} className={'logo'}>
-            <div>Logo</div>
-         </Link>
-         <nav>
-            <ul className={'navItems'}>
-               <li className="item">
-                  <NavLink to={'/'}>
-                     На початок
-                  </NavLink>
-               </li>
-               <li className={'item item_select'}>
-                  <NavLink to={'/courses'} className={'item_select_label'}>
-                     <span>Курси</span>
-                     <span className={'iconHolder'}><IoChevronDownOutline color={'#222'}/></span>
-                  </NavLink>
-                  <CoursesListModal/>
-               </li>
-               <li className="item">
-                  <NavLink to={'/contacts'}>
-                     Контакти
-                  </NavLink>
-               </li>
-            </ul>
-         </nav>
-      </motion.header>
+      <header >
+         <div className="container">
+            <Link to={'/'} className={'logo'}>
+               <div>Logo</div>
+            </Link>
+            <nav>
+               <ul className={'navItems'}>
+                  <li className="item">
+                     <NavLink to={'/'}>
+                        На початок
+                     </NavLink>
+                  </li>
+                  <li className={'item item_select'}>
+                     <NavLink to={'/courses'} className={'item_select_label'}>
+                        <span>Курси</span>
+                        <span className={'iconHolder'}><IoChevronDownOutline color={'#222'}/></span>
+                     </NavLink>
+                     <CoursesListModal/>
+                  </li>
+                  <li className="item">
+                     <NavLink to={'/contacts'}>
+                        Контакти
+                     </NavLink>
+                  </li>
+               </ul>
+            </nav>
+         </div>
+      </header>
    )
 }
