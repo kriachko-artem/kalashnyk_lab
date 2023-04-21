3214fd437fc2f88624fe226bc7b0259451e767fb
Artem Kriachko <kriachkoartem@gmail.com>
2023-04-21 17:03:33 +0300

    Replace Menu into Layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -3,22 +3,32 @@ import {Header} from "./Header/Header";
 import {Footer} from "./Footer/Footer";
 import {Main} from "./Main/Main";
 import {AnimatePresence, useScroll} from "framer-motion";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Loader} from "../components/Loader/Loader";
 import {SmallButton} from "../components/ButtonConsultation/SmallButton";
 import {ModalWrapper} from "../components/ModalWrapper/ModalWrapper";
 import {ConsultationForm} from "../components/ConsultationForm/ConsultationForm";
-import {useEffect} from "react";
+import {Menu} from "../components/Menu/Menu";
+import {hideMenu, showMenu} from "../store/slices/menu";
 
 export function Layout() {
    //Controlling page scroll by state
    const scroll = useSelector(state => state.scrollReducer.active);
    document.querySelector('body').style.overflow = scroll ? 'visible' : 'hidden';
 
+   const isMenuShowed = useSelector(state => state.menuReducer.show)
+   const dispatch = useDispatch()
+
+   const closeMenu = () => {
+      dispatch(hideMenu())
+   }
+
 
    const showConsultation = useSelector(state => state.consultationModalReducer.show);
 
    const {scrollY} = useScroll();
+
+   const coursesMenu = <div>Hello</div>;
 
    return (
       <>
@@ -33,6 +43,7 @@ export function Layout() {
                      <ConsultationForm closable={true}/>
                   </ModalWrapper>}
                </AnimatePresence>
+               {isMenuShowed && <Menu onclose={closeMenu} children={coursesMenu}/>}
             </Loader>
          </div>
       </>
