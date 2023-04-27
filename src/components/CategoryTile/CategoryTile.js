fdb2fdf31c64044de9c90342066646ec070b49ca
Artem Kriachko <kriachkoartem@gmail.com>
2023-04-27 22:51:59 +0300

    Fix scale bug and some styles update

--- src/components/CategoryTile/CategoryTile.js
+++ src/components/CategoryTile/CategoryTile.js
@@ -1,9 +1,11 @@
 import './styles/categoryTile.css'
 import {Link} from "react-router-dom";
+import {motion} from "framer-motion";
+
 
 export function CategoryTile ({data, to}) {
   return (
-     <li className="categoryTile">
+     <motion.li className="categoryTile">
         {/*<Link to={`${to || ''}${data.path}`}className={'categoryLabel'}>*/}
         {/*   LINK*/}
         {/*</Link>*/}
@@ -11,10 +13,10 @@ export function CategoryTile ({data, to}) {
           <div className="background">
             <img src="https://static.tildacdn.com/tild3736-3336-4165-b232-313963376365/2016-05-12-07-34-mak.jpg" alt={data.title}/>
           </div>
+           <span className={'categoryTile_title'}>
+             {data.title}
+           </span>
         </Link>
-        <span className={'categoryTile_title'}>
-          {data.title}
-        </span>
-     </li>
+     </motion.li>
   )
 }
